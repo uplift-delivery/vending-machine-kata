5d5c83f8ec71cc0fb6c44cc6051ffc48625cc609
Meridith Junk-Spellerberg <Meridith.Junk@gmail.com>
2023-09-01 19:26:16 +0000

    init

---
+++ e2e/src/vending-machine.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('Given', ()=> {
+   test('Should return 200', async () => {
+       const result = await fetch('http://localhost:3000');
+       const jsonResult = await result.json();
+       expect(jsonResult.test).toEqual(true)
+   });
+
+});
