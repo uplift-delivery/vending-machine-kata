ceedbfcaea0964a3e6e7548ea6ed5c7f4705026c
Meridith Junk-Spellerberg <Meridith.Junk@gmail.com>
2023-11-07 19:18:46 +0000

    fixes test

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import express from 'express';
 
-const PORT = 3000;
 const app = express();
+const port = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+app.get( "/", ( req, res ) => {
+    res.send( {"test": true});
+} );
+
+app.post( "/", ( req, res ) => {
+    res.send( {"test": true});
+});
+
+app.put( "/", ( req, res ) => {
+    res.send( {"test": true});
+});
+
+app.delete( "/", ( req, res ) => {
+    res.send( {"test": true});
+});
+
+
+app.listen(port, () => {
+    console.log(`Hello world! Running on port: ${port}.`);
+});
--- e2e/src/vending-machine.spec.ts
+++ e2e/src/vending-machine.spec.ts
@@ -1,10 +1,9 @@
 import { describe, expect, test } from '@jest/globals';
 
-describe('Given', ()=> {
+describe('Given ', ()=> {
    test('Should return 200', async () => {
        const result = await fetch('http://localhost:3000');
        const jsonResult = await result.json();
        expect(jsonResult.test).toEqual(true)
-   });
-
+   })
 });
